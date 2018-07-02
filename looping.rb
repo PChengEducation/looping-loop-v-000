17dabf85a400deb58f3317094db0703ec9104844
Pan Cheng <pchengeducation@hotmail.com>
2018-07-02 22:53:42 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,14 @@
 def looping
 	#your code here
+  counter = 0
+  loop do
+    counter += 1
+    puts "Wingardium Leviosa"
+    if counter < 1
+      break
+    end
+  end
 end
 
 #call your method here
+looping
